00319450f97028a5faa1dfdaaea98a380f1baf80
hrotkol24d <hrotkol.24d@acsjszki.hu>
2025-02-12 14:22:39 +0100

    cummit

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
+
+// kiírja a konzolra a Hello, world! szöveget
 console.log('Hello, world!');
-document.write('Hello, world!');
+
+// egysoros komment
+
+/*
+kuki
+muki
+duki
+komment...
+*/
+
+//Adattípusok
+
+//1. szöveg (string)
+console.log('ez egy szöveg típus');
+
+//2. szám (number)
+console.log(69420);
+
+//3. logikai (boolean)
+// két értéke lehet: true vagy false
+console.log(true);
+console.log(false);
+
+//4. undefined
+console.log(undefined);
+
+//5. null
+console.log(null);
+
+//Összetett adattípusok
+
+
+//6. tömb (array)
+console.log([1, 2, 3, 4, 5]);
+
+//7. objektum (object)
+console.log({
+    age: 25,
+    "firstName": "John",
+    "lastName": "Doe",
+});
